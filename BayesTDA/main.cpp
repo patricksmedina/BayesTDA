f577becb0c89004b486d62e3b00871db92912de5
Patrick S Medina <patrick.medina1@gmail.com>
2017-06-28 09:47:45 -0500

    Initial Commit

---
+++ BayesTDA/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BayesTDA
+//
+//  Created by Patrick Medina on 6/28/17.
+//  Copyright © 2017 Patrick Medina. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
